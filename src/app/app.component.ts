ec60a23af1fa748f66679cdd0f82822d4971bd74
Daniel Richter <danielrichter@posteo.de>
2020-11-01 10:36:10 +0100

    feat: draw tree graph with svg
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,105 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ORIENTATION} from './landscape/landscape.component';
 
 @Component({
   selector: 'app-root',
-  template: `<app-landscape></app-landscape>`,
-  styles: []
+  template: `
+    <div>
+      Orientation
+      <label class="switch">
+        <input type="checkbox" (change)="this.change()">
+        <span class="slider round"></span>
+      </label>
+      {{orientation}}
+    </div>
+    <app-landscape [orientation]="orientation"></app-landscape>`,
+  styles: [`
+    :host {
+      /*background-color: dodgerblue;*/
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    div {
+      /*background-color: tomato;*/
+    }
+
+    app-landscape {
+      flex-grow: 1;
+    }
+
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 60px;
+      height: 34px;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+
+    /* The slider */
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+
+    input:checked + .slider {
+      background-color: #2196F3;
+    }
+
+    input:focus + .slider {
+      box-shadow: 0 0 1px #2196F3;
+    }
+
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 34px;
+    }
+
+    .slider.round:before {
+      border-radius: 50%;
+    }
+  `]
 })
 export class AppComponent {
+  public orientation: ORIENTATION = 'horizontal';
+
+  change(): void {
+    if (this.orientation === 'vertical') {
+      this.orientation = 'horizontal';
+    }else{
+      this.orientation = 'vertical';
+    }
+  }
 }
--- src/app/landscape/landscape.component.ts
+++ src/app/landscape/landscape.component.ts
@@ -1,15 +1,225 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-landscape',
-  templateUrl: './landscape.component.html',
-  styleUrls: ['./landscape.component.scss']
+  template: `
+    <svg #svg>
+      <g attr.transform="translate({{margin}},{{margin}})">
+        <!--        <line class="line" *ngFor="let link of links"-->
+        <!--              [attr.x1]="this.rotateCoordinates(this.orientation)(link.source).x"-->
+        <!--              [attr.x2]="this.rotateCoordinates(this.orientation)(link.target).x"-->
+        <!--              [attr.y1]="this.rotateCoordinates(this.orientation)(link.source).y"-->
+        <!--              [attr.y2]="this.rotateCoordinates(this.orientation)(link.target).y"></line>-->
+        <g *ngFor="let link of links">
+<!--                    <circle attr.cx="{{this.rotateCoordinates(this.orientation)(link.controlSource).x}}" attr.cy="{{this.rotateCoordinates(this.orientation)(link.controlSource).y}}" fill="blue" r="5"></circle>-->
+<!--                    <circle attr.cx="{{this.rotateCoordinates(this.orientation)(link.controlTarget).x}}" attr.cy="{{this.rotateCoordinates(this.orientation)(link.controlTarget).y}}" fill="green" r="5"></circle>-->
+          <!--          <path class="line" [attr.d]='"M" + this.rotateCoordinates(this.orientation)(link.source).x + "," + this.rotateCoordinates(this.orientation)(link.source).y + "A" + link.curve.dr + "," + link.curve.dr + " 0 0,1 " + this.rotateCoordinates(this.orientation)(link.target).x + "," + this.rotateCoordinates(this.orientation)(link.target).y'></path>-->
+          <path class="line"
+                attr.d="M{{this.rotateCoordinates(this.orientation)(link.source).x}} {{this.rotateCoordinates(this.orientation)(link.source).y}}
+                 C {{this.rotateCoordinates(this.orientation)(link.controlSource).x}} {{this.rotateCoordinates(this.orientation)(link.controlSource).y}}
+                 {{this.rotateCoordinates(this.orientation)(link.controlTarget).x}} {{this.rotateCoordinates(this.orientation)(link.controlTarget).y}}
+                 {{this.rotateCoordinates(this.orientation)(link.target).x}} {{this.rotateCoordinates(this.orientation)(link.target).y}}
+                 "
+          ></path>
+        </g>
+        <g *ngFor="let node of nodes">
+          <circle
+            [attr.cx]="this.rotateCoordinates(this.orientation)(node).x"
+            [attr.cy]="this.rotateCoordinates(this.orientation)(node).y"
+            [attr.r]="radius"></circle>
+          <text [attr.x]="this.rotateCoordinates(this.orientation)(node).x" [attr.dx]="-radius/2-14" [attr.dy]="+radius+12" a
+                [attr.y]="this.rotateCoordinates(this.orientation)(node).y" fill="black">{{node.data.name}}</text>
+        </g>
+      </g>
+    </svg>
+  `,
+  styles: [`
+    :host {
+      display: block;
+      /*background-color: aqua;*/
+    }
+
+    svg {
+      /*background-color: rebeccapurple;*/
+      height: 100%;
+      width: 100%;
+      /*border: 1px solid red;*/
+    }
+
+    .line {
+      stroke-width: 2;
+      stroke: black;
+      fill: transparent;
+    }
+
+    text {
+      z-index: 999;
+    }
+  `]
 })
-export class LandscapeComponent implements OnInit {
+export class LandscapeComponent implements OnInit, OnChanges {
+  public radius = 30;
+  public margin = this.radius * 2;
+  private data = {
+    name: 'BMW Neue Klasse',
+    children: [
+      {
+        name: 'BMW E21',
+        children: [
+          {
+            name: 'BMW E30',
+            children: [
+              {
+                name: 'BMW E36',
+                children: [
+                  {
+                    name: 'BMW E46',
+                    children: [
+                      {
+                        name: 'BMW E92',
+                        children: [
+                          {
+                            name: 'BMW F32',
+                            children: [
+                              {
+                                name: 'BMW G22'
+                              }
+                            ]
+                          },
+                          {
+                            name: 'BMW F30',
+                            children: [
+                              {
+                                name: 'BMW G20'
+                              }
+                            ]
+                          }
+                        ]
+                      },
+                      {
+                        name: 'BMW E81',
+                        children: [
+                          {
+                            name: 'BMW F20',
+                            children: [
+                              {
+                                name: 'BMW F40'
+                              }
+                            ]
+                          },
+                          {
+                            name: 'BMW F22'
+                          }
+                        ]
+                      },
+                    ]
+                  },
+                ]
+              },
+            ]
+          },
+        ]
+      },
+      {
+        name: 'BMW E12',
+        children: [
+          {
+            name: 'BMW E28',
+            children: [
+              {
+                name: 'BMW E34',
+                children: [
+                  {
+                    name: 'BMW E39',
+                    children: [
+                      {
+                        name: 'BMW E60',
+                        children: [
+                          {
+                            name: 'BMW F10',
+                            children: [
+                              {
+                                name: 'BMW G30'
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'BMW E24',
+        children: [
+          {
+            name: 'BMW E63',
+            children: [
+              {
+                name: 'BMW F12'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  public root = d3.hierarchy(this.data);
+  public nodes = [];
+  public links = [];
+  private size: [number, number] = [0, 0];
+  @Input() orientation: ORIENTATION;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild('svg', {static: true}) canvas;
+  public rotateCoordinates = (rotation: ORIENTATION) => (coordinates: { x: number, y: number }): { x: number, y: number } => rotation === 'vertical' ? coordinates : {
+    x: coordinates.y,
+    y: coordinates.x
   }
 
+  ngOnInit(): void {
+    this.size = [
+      this.canvas.nativeElement.getBoundingClientRect().width - this.margin * 2,
+      this.canvas.nativeElement.getBoundingClientRect().height - this.margin * 2,
+    ];
+    this.updateTree();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateTree();
+  }
+
+  updateTree(): void {
+    const treelayout = d3.tree();
+    treelayout.size(rotateSize(this.orientation)(this.size));
+    treelayout(this.root);
+    this.nodes = this.root.descendants();
+    this.links = this.root.links().map(d => {
+      const dx = d.target['x'] - d.source['x'];
+      const dy = d.target['y'] - d.source['y'];
+      const dr = Math.sqrt(dx * dx + dy * dy);
+      return ({
+        ...d,
+        // Blue
+        controlSource: {
+          x: d.source['x'],
+          y: (d.source['y']+d.target['y'])/2,
+        },
+        // Green
+        controlTarget: {
+          x: d.target['x'],
+          y: (d.source['y']+d.target['y'])/2,
+        }
+      });
+    });
+  }
 }
+
+export declare type ORIENTATION = 'vertical' | 'horizontal';
+
+
+const rotateSize = (rotation: ORIENTATION) => (size: [number, number]): [number, number] => rotation === 'vertical' ? size : [size[1], size[0]];
